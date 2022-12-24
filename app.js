0469520a2bd7b7dd2cf8feb4645ea0ee85e05121
Eadson Technologies <eadsontech@gmail.com>
2022-12-24 06:02:33 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const btn = document.querySelector(".btn");
+const content = document.querySelector(".content");
+const img = document.querySelector(".container img");
+
+const URL = 'https://api.chucknorris.io/jokes/random';
+
+btn.addEventListener("click", async()=>{
+    try {
+        const data = await fetch(URL);
+const response = await data.json();
+displayData(response);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// function getData(url){
+//     return new Promise((resolve, reject) =>{
+// const xhr = new XMLHttpRequest();
+//     xhr.open("GET", url);
+//     xhr.send();
+//     xhr.onreadystatechange = function(){
+//         if(xhr.readyState !== 4) return;
+//         if(xhr.status ===200){
+//             resolve(xhr.responseText)
+//         } else{
+//             reject({
+//                 status: xhr.status,
+//                 text: xhr.statusText,
+                
+//             });
+//         }
+//     }
+//     });
+    
+// }
+
+function displayData({value:joke}){
+    img.classList.add("shake-img");
+            // const { value:joke } = data;
+            content.textContent = joke;
+            const random = Math.random() * 1000;
+            setTimeout(() =>{
+            img.classList.remove("shake-img");
+            },random);
+}
